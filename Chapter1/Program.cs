271eac18e090720a503a2fc7466459033f64b48d
Krishnan Nath <krishnan@me.com>
2016-02-25 23:14:23 +0000

    Listing 1-4: Stopping a thread

--- Chapter1/Program.cs
+++ Chapter1/Program.cs
@@ -5,25 +5,22 @@ namespace Chapter1
 {
 	public static class Program
 	{
-		public static void ThreadMethod(object o)
-		{
-			for (int i = 0; i < (int)o; i++) {
-				Console.WriteLine ("ThreadProc: {0}", i);
-				Thread.Sleep(1000);
-			}
-		}
 		public static void Main (string[] args)
 		{
-			var t = new Thread (new ParameterizedThreadStart (ThreadMethod));
+			var stopped = false;
+			var t = new Thread (new ThreadStart (() => {
+				while (!stopped) {
+					Console.WriteLine ("Running ...");
+					Thread.Sleep (1000);
+				}
+			}));
 
-			t.Start (5);
-			/*
-			for (int i = 0; i < 4; i++) {
-				Console.WriteLine ("Main Thread: Do some work");
-				Thread.Sleep (0);
-			}
+			t.Start ();
+			Console.WriteLine ("Press any key to exit");
+			Console.ReadKey ();
 
-			*/
-			t.Join ();}
+			stopped = true;
+			t.Join ();
+		}
 	}
 }
