cf482690cec8f9fa247a0de4f226afa0e3def460
Krish Nath <krishnan.n@mmtdigital.co.uk>
2016-02-26 13:51:28 +0000

    Listing 1-5: Using the ThreadStaticAttribute

--- Chapter1/Program.cs
+++ Chapter1/Program.cs
@@ -3,24 +3,33 @@ using System.Threading;
 
 namespace Chapter1
 {
-	public static class Program
-	{
-		public static void Main (string[] args)
-		{
-			var stopped = false;
-			var t = new Thread (new ThreadStart (() => {
-				while (!stopped) {
-					Console.WriteLine ("Running ...");
-					Thread.Sleep (1000);
-				}
-			}));
+    public static class Program
+    {
+        [ThreadStatic]
+        public static int _field;
 
-			t.Start ();
-			Console.WriteLine ("Press any key to exit");
-			Console.ReadKey ();
+        public static void Main(string[] args)
+        {
+             new Thread(() =>
+            {
+                for (int i = 0; i<10; i++)
+                {
+                    _field++;
+                    Console.WriteLine("Thread A: {0}", _field);
 
-			stopped = true;
-			t.Join ();
-		}
-	}
+                }
+            }).Start();
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    _field++;
+                    Console.WriteLine("Thread B: {0}", _field);
+
+                }
+            }).Start();
+            Console.ReadKey();
+        }
+    }
 }
