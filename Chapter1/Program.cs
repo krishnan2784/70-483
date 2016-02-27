de4440ea1159b19926c290a081610f2c098479b9
Krishnan Nath <krishnan@me.com>
2016-02-27 00:36:09 +0000

    Listing 1-7: Queuing some work to the thread pool

--- Chapter1/Program.cs
+++ Chapter1/Program.cs
@@ -5,28 +5,13 @@ namespace Chapter1
 {
     public static class Program
     {
-        private static ThreadLocal<int> _field = new ThreadLocal<int>(() => Thread.CurrentThread.ManagedThreadId);
-
         public static void Main(string[] args)
         {
-             new Thread(() =>
-            {
-                for (var x = 0; x< _field.Value; x++)
-                {
-                    Console.WriteLine("Thread A: {0}", x);
-
-                }
-            }).Start();
-
-            new Thread(() =>
-            {
-                for (var x = 0; x < _field.Value; x++)
-                {
-                    Console.WriteLine("Thread B: {0}", x);
-
-                }
-            }).Start();
-            Console.ReadKey();
+			ThreadPool.QueueUserWorkItem((s) =>
+			{
+					Console.WriteLine("Working on a thread from threadpool");
+			});
+			Console.ReadLine ();
         }
     }
 }
