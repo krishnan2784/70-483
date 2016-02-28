eed30308479b8dd149561e9a87d1b3414f398459
Krishnan Nath <krishnan@me.com>
2016-02-28 23:04:17 +0000

    Listing 1-15: Using Task.WaitAny

--- Chapter1/Program.cs
+++ Chapter1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,23 +9,29 @@ namespace Chapter1
 	{
 		public static void Main (string[] args)
 		{
-			var tasks = new Task[3];
+			Task<int>[] tasks = new Task<int>[3];
 
 			tasks [0] = Task.Run (() => {
-				Thread.Sleep(1000);
-				Console.WriteLine("1");
+				Thread.Sleep (2000);
 				return 1;
 			});
 			tasks [1] = Task.Run (() => {
-				Thread.Sleep(1000);
-				Console.WriteLine("2");
+				Thread.Sleep (1000);
 				return 2;
 			});
 			tasks [2] = Task.Run (() => {
-				Thread.Sleep(1000);
-				Console.WriteLine("3");
+				Thread.Sleep (3000);
 				return 3;
 			});
+			while (tasks.Length > 0) {
+				int i = Task.WaitAny (tasks);
+				Task<int> completedTask = tasks[i];
+				Console.WriteLine (completedTask.Result);
+				var temp = tasks.ToList ();
+				temp.RemoveAt (i);
+				tasks = temp.ToArray ();
+
+			}
 			Task.WaitAll (tasks);
 		}
 	}
