7099c57ec6c0ba7703a59b855fe0a4531baa1dbc
Krishnan Nath <krishnan@me.com>
2016-02-29 22:48:55 +0000

    Listing 1-27: Catching AggregateException

--- Chapter1/Program.cs
+++ Chapter1/Program.cs
@@ -14,15 +14,21 @@ namespace Chapter1
 		public static void Main (string[] args)
 		{
 			var numbers = Enumerable.Range (0, 20);
-
+			try
+			{
 				var parallelResult = numbers.AsParallel().Where(i=>IsEven(i));
 				parallelResult.ForAll(d=> Console.WriteLine(d));
-
+			}
+			catch(AggregateException e) 
+			{
+				Console.WriteLine ("There were {0} exceptions", e.InnerExceptions.Count);
+			}
 		}
 
 		public static bool IsEven(int i)
 		{
-			
+			if (i % 10 == 0)
+				throw new ArgumentException ("i");
 			return i % 2 == 0;
 		}
 	    
