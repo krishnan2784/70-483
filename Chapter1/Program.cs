0ba98096423cf9198c5c0038aee1d6c06f49690d
Krishnan Nath <krishnan@me.com>
2016-02-29 22:26:04 +0000

    Listing 1-22: Using AsParallel

--- Chapter1/Program.cs
+++ Chapter1/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Linq;
 
 namespace Chapter1
 {
@@ -12,42 +13,11 @@ namespace Chapter1
 	{
 		public static void Main (string[] args)
 		{
-		    var result = DownloadContent().Result;
-            Console.WriteLine(result);
+			var number = Enumerable.Range (0, 1000000000);
+			var parallelResult = number.AsParallel ().Where (i => i % 2 == 0).ToArray ();
+			Console.WriteLine ("Number of values:{0}", parallelResult.Length);
 		}
 
-	    public static async Task<string> DownloadContent()
-	    {
-	        using (HttpClient client = new HttpClient())
-	        {
-	            var result = await client.GetStringAsync("http://www.microsoft.com");
-                return result;
-	        }
-	    }
-
-	    public static Task SleepAsyncA(int milliSecondsTimeout)
-	    {
-	        return Task.Run(() => Thread.Sleep(milliSecondsTimeout));
-	    }
-
-        public static Task SleepAsyncB(int milliSecondsTimeout)
-        {
-            TaskCompletionSource<bool> taskCompletionSource = null;
-            var t = new Timer(delegate { taskCompletionSource.TrySetResult(true); }, null, -1, -1);
-            taskCompletionSource= new TaskCompletionSource<bool>(t);
-            t.Change(milliSecondsTimeout, -1);
-            return taskCompletionSource.Task;
-        }
-
-	    /*private static async void Button_Click(object sender, RoutedEventArgs e)
-	    {
-            var httpClient = new HttpClient();
-	        var content = await httpClient.GetStringAsync("http://www.microsoft.com").ConfigureAwait(false);
-            using (var sourceStream = new FileStream("temp.html", FileMode.Create, FileAccess.Write, FileShare.None, 4096, useAsync:true))
-	        {
-	            var encodedText = Encoding.Unicode.GetByteCount(content);
-	        }
-
-	    }*/
+	    
 	}
 }
