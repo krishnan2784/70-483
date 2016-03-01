979ff43e36b6f83551381d0df7eb039aaa9f7f38
Krish Nath <krishnan.n@mmtdigital.co.uk>
2016-03-01 13:14:06 +0000

    Resharper Tidy

--- Chapter1/Program.cs
+++ Chapter1/Program.cs
@@ -9,12 +9,13 @@ namespace Chapter1
     {
         public static void Main(string[] args)
         {
+            if (args == null) throw new ArgumentNullException(nameof(args));
             var stack = new ConcurrentStack<int>();
             stack.Push(42);
             int result;
             if(stack.TryPop(out result))
                 Console.WriteLine("Popped: {0}", result);
-            stack.PushRange(new int[] {1, 2, 3, 4});
+            stack.PushRange(new[] {1, 2, 3, 4});
 
             var values = new int[2];
             stack.TryPopRange(values);
