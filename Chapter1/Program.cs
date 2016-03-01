01ce2044a2d730a4a9e79c93002ea4ee00821f98
Krish Nath <krishnan.n@mmtdigital.co.uk>
2016-03-01 12:49:38 +0000

    Listing 1-28: Using BlockingCollection<T>.

--- Chapter1/Program.cs
+++ Chapter1/Program.cs
@@ -1,36 +1,32 @@
 ﻿using System;
-using System.IO;
-using System.Net.Http;
-using System.Text;
-using System.Threading;
+using System.Collections.Concurrent;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Linq;
 
 namespace Chapter1
 {
 	public static class Program
 	{
-		public static void Main (string[] args)
-		{
-			var numbers = Enumerable.Range (0, 20);
-			try
-			{
-				var parallelResult = numbers.AsParallel().Where(i=>IsEven(i));
-				parallelResult.ForAll(d=> Console.WriteLine(d));
-			}
-			catch(AggregateException e) 
-			{
-				Console.WriteLine ("There were {0} exceptions", e.InnerExceptions.Count);
-			}
-		}
+        public static void Main(string[] args)
+        {
+            var col = new BlockingCollection<string>();
+            var read = Task.Run(() =>
+            {
+                while (true)
+                {
+                    Console.WriteLine(col.Take());
+                }
+            });
 
-		public static bool IsEven(int i)
-		{
-			if (i % 10 == 0)
-				throw new ArgumentException ("i");
-			return i % 2 == 0;
-		}
-	    
-	}
+            var write = Task.Run(() =>
+            {
+                while (true)
+                {
+                    var s = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(s)) break;
+                    col.Add(s);
+                }
+            });
+            write.Wait();
+        }
+    }
 }
