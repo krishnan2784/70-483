d83bdc45ad8fa1bc7e75f7575295730ca631242c
Krish Nath <krishnan.n@mmtdigital.co.uk>
2016-04-25 16:29:57 +0100

    Listing 1-33: Using a ConcurrentQueue.

--- Chapter1/Program.cs
+++ Chapter1/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Chapter1
 {
@@ -10,20 +8,16 @@ namespace Chapter1
         public static void Main(string[] args)
         {
             if (args == null) throw new ArgumentNullException(nameof(args));
-            var stack = new ConcurrentStack<int>();
-            stack.Push(42);
-            int result;
-            if(stack.TryPop(out result))
-                Console.WriteLine("Popped: {0}", result);
-            stack.PushRange(new[] {1, 2, 3, 4});
+            var queue = new ConcurrentQueue<int>();
+            queue.Enqueue(42);
 
-            var values = new int[2];
-            stack.TryPopRange(values);
-            foreach (var i in values)
-            {
-                Console.WriteLine(i);
-            }
+            int result;
+            if(queue.TryDequeue(out result))
+                    Console.WriteLine("Dequeued: {0}", result );
+
             Console.ReadKey();
         }
+
+
     }
 }
