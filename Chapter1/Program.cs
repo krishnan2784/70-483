29a11a562b9472fc233c915af7f13a74aa6be5db
Surendra Nath <krishnan2784@gmail.com>
2016-07-19 11:04:29 +0100

    Listing 1-42: Using a CancellationToken

--- Chapter1/Program.cs
+++ Chapter1/Program.cs
@@ -7,22 +7,27 @@ namespace Chapter1
     public static class Program
     {
         private static int _value = 1;
+
         public static void Main(string[] args)
         {
-            var t1 = Task.Run(() =>
-            {
-                if (_value != 1) return;
-                Thread.Sleep(1000);
-                _value = 2;
-            });
-            var t2 = Task.Run(() =>
-            {
-                _value = 3;
-            });
-            Task.WaitAll(t1, t2);
-            Console.WriteLine(_value);
-            
-            Console.ReadKey();
+            var cancellationTokenSource = new CancellationTokenSource();
+            var token = cancellationTokenSource.Token;
+            var taks = Task.Run(() =>
+                {
+                    while (!token.IsCancellationRequested)
+                    {
+                        Console.WriteLine("*");
+                        Thread.Sleep(1000); 
+                    }
+                }, token);
+
+
+            Console.WriteLine("Press enter to stop the task");
+            Console.ReadLine();
+            cancellationTokenSource.Cancel();
+
+            Console.WriteLine("Press enter to end the application");
+            Console.ReadLine();
         }
     }
 }
