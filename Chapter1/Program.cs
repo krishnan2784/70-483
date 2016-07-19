1f39151b62cb25c72133304538b812e187edb2ee
Surendra Nath <krishnan2784@gmail.com>
2016-07-19 11:27:28 +0100

    Listing 1-43: Throwing OperationCanceledException

--- Chapter1/Program.cs
+++ Chapter1/Program.cs
@@ -6,25 +6,34 @@ namespace Chapter1
 {
     public static class Program
     {
-        private static int _value = 1;
 
         public static void Main(string[] args)
         {
             var cancellationTokenSource = new CancellationTokenSource();
             var token = cancellationTokenSource.Token;
-            var taks = Task.Run(() =>
+            var task = Task.Run(() =>
                 {
                     while (!token.IsCancellationRequested)
                     {
                         Console.WriteLine("*");
-                        Thread.Sleep(1000); 
+                        Thread.Sleep(1000);
                     }
+                    token.ThrowIfCancellationRequested();
                 }, token);
 
 
-            Console.WriteLine("Press enter to stop the task");
-            Console.ReadLine();
-            cancellationTokenSource.Cancel();
+            try
+            {
+                Console.WriteLine("Press enter to stop the task");
+                Console.ReadLine();
+                cancellationTokenSource.Cancel();
+
+
+            }
+            catch (AggregateException exception)
+            {
+                Console.WriteLine(exception.InnerExceptions[0].Message);
+            }
 
             Console.WriteLine("Press enter to end the application");
             Console.ReadLine();
